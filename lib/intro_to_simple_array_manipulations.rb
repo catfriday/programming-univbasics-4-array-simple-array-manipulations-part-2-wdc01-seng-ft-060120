91b7783914f1df46d414a9a95bbdaa3c97c66f74
Catrina Friday <cat.friday@gmail.com>
2020-05-01 15:44:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,8 +18,8 @@ def using_delete (array, string)
   array.delete(string)
 end
 
-def using_delete_at (array, "5")
-  array.delete_at (1)
+def using_delete_at (array, integer)
+  array.delete_at(1)
 end
 
 
